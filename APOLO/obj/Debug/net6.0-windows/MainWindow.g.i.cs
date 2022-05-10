6dcbd4a959af78f74b44efde17f7f6c1125391d0
Julio Sierra <juliosierra_01@hotmail.com>
2022-05-10 07:58:56 -0400

    .Name2

--- APOLO/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ APOLO/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -773,7 +773,7 @@ namespace APOLO {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/APOLO;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/APOLO;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
