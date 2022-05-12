ec851453f8c8676ee785fdeb7d29c5afe444a255
Julio Sierra <juliosierra299@gmail.com>
2022-05-11 23:06:36 -0400

    Update 4

--- APOLO/MainWindow.xaml.cs
+++ APOLO/MainWindow.xaml.cs
@@ -369,7 +369,24 @@ namespace APOLO
             {
                 RowMAT501.Fill = new SolidColorBrush(Colors.LightBlue);
             }
-
+            //MED-750
+            if(Convert.ToBoolean(MED750.IsChecked))
+            {
+                RowMED750.Fill= new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowMED750.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            //MED-755
+            if(Convert.ToBoolean(MED755.IsChecked))
+            {
+                RowMED755.Fill= new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowMED755.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
 
             //CUARTO CUATRIMESTRE
             //IEL-100 y IEL-105
@@ -397,7 +414,287 @@ namespace APOLO
             {
                 RowIEL105.Fill = new SolidColorBrush(Colors.LightBlue);
             }
+            //INF-171, INF-172, INF-173, INF-385 Y INF-387
+            if (Convert.ToBoolean(INF121.IsChecked)&&Convert.ToBoolean(INF167.IsChecked)&&Convert.ToBoolean(INF168.IsChecked))
+            {
+                INF171.IsEnabled=true;
+                RowINF171.Fill= new SolidColorBrush(Colors.LightGreen);
+                INF172.IsEnabled=true;
+                RowINF172.Fill= new SolidColorBrush(Colors.LightGreen);
+                INF173.IsEnabled = true;
+                RowINF173.Fill = new SolidColorBrush(Colors.LightGreen);
+                INF385.IsEnabled = true;
+                RowINF385.Fill = new SolidColorBrush(Colors.LightGreen);
+                INF387.IsEnabled = true;
+                RowINF387.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                INF171.IsEnabled = false;
+                INF171.IsChecked = false;
+                RowINF171.Fill = new SolidColorBrush(Colors.Salmon);
+                INF172.IsEnabled = false;
+                INF172.IsChecked = false;
+                RowINF172.Fill = new SolidColorBrush(Colors.Salmon);
+                INF173.IsEnabled = false;
+                INF173.IsChecked = false;
+                RowINF173.Fill = new SolidColorBrush(Colors.Salmon);
+                INF385.IsEnabled = false;
+                INF385.IsChecked = false;
+                RowINF385.Fill = new SolidColorBrush(Colors.Salmon);
+                INF387.IsEnabled = false;
+                INF387.IsChecked = false;
+                RowINF387.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if(Convert.ToBoolean(INF171.IsChecked))
+            {
+                RowINF171.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF172.IsChecked))
+            {
+                RowINF172.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF173.IsChecked))
+            {
+                RowINF173.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF385.IsChecked))
+            {
+                RowINF385.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF387.IsChecked))
+            {
+                RowINF387.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //ING-165
+            if (Convert.ToBoolean(ING125.IsChecked))
+            {
+                ING165.IsEnabled = true;
+                RowING165.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                ING165.IsEnabled = false;
+                ING165.IsChecked = false;
+                RowING165.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(ING165.IsChecked))
+            {
+                RowING165.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //MAT-350
+            if (Convert.ToBoolean(MAT340.IsChecked))
+            {
+                MAT350.IsEnabled = true;
+                RowMAT350.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                MAT350.IsEnabled = false;
+                MAT350.IsChecked = false;
+                RowMAT350.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(MAT350.IsChecked))
+            {
+                RowMAT350.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
 
+            //QUINTO CUATRIMESTRE
+            //DIB-520
+            if (Convert.ToBoolean(DIB520.IsChecked))
+            {
+                RowDIB520.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowDIB520.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            //IID-420
+            if (Convert.ToBoolean(IID420.IsChecked))
+            {
+                RowIID420.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowIID420.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            //INF-440 y INF-445
+            if (Convert.ToBoolean(IEL100.IsChecked) && Convert.ToBoolean(IEL105.IsChecked) && Convert.ToBoolean(INF204.IsChecked))
+            {
+                INF440.IsEnabled = true;
+                RowINF440.Fill = new SolidColorBrush(Colors.LightGreen);
+                INF445.IsEnabled = true;
+                RowINF445.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                INF440.IsEnabled = false;
+                INF440.IsChecked = false;
+                RowINF440.Fill = new SolidColorBrush(Colors.Salmon);
+                INF445.IsEnabled = false;
+                INF445.IsChecked = false;
+                RowINF445.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(INF440.IsChecked))
+            {
+                RowINF440.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF445.IsChecked))
+            {
+                RowINF445.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //INF-481, INF-482 y INF-535
+            if (Convert.ToBoolean(INF171.IsChecked) && Convert.ToBoolean(INF385.IsChecked) && Convert.ToBoolean(INF387.IsChecked))
+            {
+                INF481.IsEnabled=true;
+                RowINF481.Fill = new SolidColorBrush(Colors.LightGreen);
+                INF482.IsEnabled=true;
+                RowINF482.Fill= new SolidColorBrush(Colors.LightGreen);
+                INF535.IsEnabled=true;
+                RowINF535.Fill= new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                INF481.IsEnabled = false;
+                INF481.IsChecked=false;
+                RowINF481.Fill = new SolidColorBrush(Colors.Salmon);
+                INF482.IsEnabled = false;
+                INF482.IsChecked = false;
+                RowINF482.Fill = new SolidColorBrush(Colors.Salmon);
+                INF535.IsEnabled = false;
+                INF535.IsChecked = false;
+                RowINF535.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(INF481.IsChecked))
+            {
+                RowINF481.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF482.IsChecked))
+            {
+                RowINF482.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF535.IsChecked))
+            {
+                RowINF535.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //MAT-360
+            if (Convert.ToBoolean(MAT350.IsChecked))
+            {
+                MAT360.IsEnabled = true;
+                RowMAT360.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                MAT360.IsEnabled = false;
+                MAT360.IsChecked = false;
+                RowMAT360.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(MAT360.IsChecked))
+            {
+                RowMAT360.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //SOC-700
+            if (Convert.ToBoolean(SOC700.IsChecked))
+            {
+                RowSOC700.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowSOC700.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+
+            //SEXTO CUATRIMESTRE
+            //INF-184 Y INF-185
+            if (Convert.ToBoolean(INF440.IsChecked)&& Convert.ToBoolean(INF445.IsChecked))
+            {
+                INF184.IsEnabled = true;
+                RowINF184.Fill= new SolidColorBrush(Colors.LightGreen);
+                INF185.IsEnabled = true;
+                RowINF185.Fill= new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                INF184.IsEnabled = false;
+                INF184.IsChecked = false;
+                RowINF184.Fill = new SolidColorBrush(Colors.Salmon);
+                INF185.IsEnabled = false;
+                INF185.IsChecked = false;
+                RowINF185.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(INF184.IsChecked))
+            {
+                RowINF184.Fill= new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF185.IsChecked))
+            {
+                RowINF185.Fill= new SolidColorBrush(Colors.LightBlue);
+            }
+            //INF-213 Y INF-214
+            if (Convert.ToBoolean(INF172.IsChecked) && Convert.ToBoolean(INF173.IsChecked))
+            {
+                INF213.IsEnabled = true;
+                RowINF213.Fill= new SolidColorBrush(Colors.LightGreen);
+                INF214.IsEnabled = true;
+                RowINF214.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                INF213.IsEnabled = false;
+                INF213.IsChecked = false;
+                RowINF213.Fill = new SolidColorBrush(Colors.Salmon);
+                INF214.IsEnabled = false;
+                INF214.IsChecked = false;
+                RowINF214.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if (Convert.ToBoolean(INF213.IsChecked))
+            {
+                RowINF213.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            if (Convert.ToBoolean(INF214.IsChecked))
+            {
+                RowINF214.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //INF-225 y INF-331
+            if(Convert.ToBoolean(ESP095.IsChecked) && Convert.ToBoolean(INF117.IsChecked) && Convert.ToBoolean(ING105.IsChecked) && Convert.ToBoolean(MAT115.IsChecked) && Convert.ToBoolean(MAT160.IsChecked) && Convert.ToBoolean(ORI112.IsChecked) && Convert.ToBoolean(SOC116.IsChecked) && Convert.ToBoolean(SOC180.IsChecked))
+            {
+                if (Convert.ToBoolean(ESP181.IsChecked) && Convert.ToBoolean(INF164.IsChecked) && Convert.ToBoolean(INF165.IsChecked) && Convert.ToBoolean(INF204.IsChecked) && Convert.ToBoolean(ING115.IsChecked) && Convert.ToBoolean(MAT170.IsChecked) && Convert.ToBoolean(MAT190.IsChecked) && Convert.ToBoolean(MAT191.IsChecked) && Convert.ToBoolean(SOC150.IsChecked))
+                {
+                    if (Convert.ToBoolean(ESP189.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF167.IsChecked) && Convert.ToBoolean(INF168.IsChecked) && Convert.ToBoolean(ING125.IsChecked) && Convert.ToBoolean(MAT340.IsChecked) && Convert.ToBoolean(MAT500.IsChecked) && Convert.ToBoolean(MAT501.IsChecked) && Convert.ToBoolean(MED750.IsChecked) && Convert.ToBoolean(MED755.IsChecked) &&)
+                    {
+                        if (Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked) && Convert.ToBoolean(INF121.IsChecked))
+                        {
+                            if()
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                INF225.IsEnabled=false;
+                RowINF225.Fill = new SolidColorBrush(Colors.Salmon);
+                INF331.IsEnabled = false;
+                RowINF331.Fill = new SolidColorBrush(Colors.Salmon);
+            }
+            if(Convert.ToBoolean(INF225.IsChecked))
+            {
+                RowINF225.Fill= new SolidColorBrush(Colors.LightBlue);
+            }
+            if(Convert.ToBoolean(INF331.IsChecked))
+            {
+                RowINF331.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            //SOC-140
+            if (Convert.ToBoolean(SOC140.IsChecked))
+            {
+                RowSOC140.Fill = new SolidColorBrush(Colors.LightBlue);
+            }
+            else
+            {
+                RowSOC140.Fill = new SolidColorBrush(Colors.LightGreen);
+            }
         }
     }
 }
