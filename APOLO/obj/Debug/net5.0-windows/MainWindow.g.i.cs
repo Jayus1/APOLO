2c2bffeecdad3dc992f8ec370706fe5c09d61018
Julio Sierra <juliosierra_01@hotmail.com>
2022-08-01 09:36:00 -0400

    Fixing bugs 2

--- APOLO/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ APOLO/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -1493,7 +1493,7 @@ namespace APOLO {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/APOLO;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/APOLO;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
